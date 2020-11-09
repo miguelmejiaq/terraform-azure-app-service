28f1b03c340de2c869b9869f8176905464340975
miguelmejiaq@outlook.com <miguelmejiaq@outlook.com>
2020-11-09 13:01:20 -0500

    Fixed variable Name

--- modules/resource-group/main.tf
+++ modules/resource-group/main.tf
@@ -7,7 +7,7 @@
 #   @name: Name of the Resource Group
 #   @id: ID of the Resource Group
 resource "azurerm_resource_group" "app-service-resource-group" {
-    name = "${var.var_ResourceIdentifier}rsg"
+    name = "${var.var_resourceIdentifier}rsg"
     location = "${var.var_resourceLocation}"
     tags = {
         for k,v in merge(
