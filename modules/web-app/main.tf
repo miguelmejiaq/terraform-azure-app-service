804f639b23ddf20119413eef118a166740a1895d
miguelmejiaq@outlook.com <miguelmejiaq@outlook.com>
2020-11-09 19:19:57 -0500

    added = to app_settings

--- modules/web-app/main.tf
+++ modules/web-app/main.tf
@@ -3,18 +3,15 @@ resource "azurerm_app_service" "app-service-azure-web-app" {
     resource_group_name = "${var.var_resourceGroupName}"
     location = "${var.var_resourceLocation}"
     app_service_plan_id = "${var.var_appServicePlanId}"
-
-    app_settings {
+    site_config {
+        linux_fx_version = "DOCKER|${var.var_containerUrl}/${var.var_containerRepository}:${var.var_containerTag}"
+    }
+    app_settings = {
         WEBSITES_ENABLE_APP_SERVICE_STORAGE = false
         DOCKER_REGISTRY_SERVER_URL = "${var.var_containerUrl}"
         DOCKER_REGISTRY_SERVER_USERNAME = "${var.var_containerUser}"
         DOCKER_REGISTRY_SERVER_PASSWORD = "${var.var_containerPassword}"
     }
-
-    site_config {
-        linux_fx_version = "DOCKER|${var.var_containerUrl}/${var.var_containerRepository}:${var.var_containerTag}"
-    }
-    
     identity {
         type = "SystemAssigned"
     }
